2817225efd568a56193b489d60b55191c25642d9
JNS <65025344+jinto4638@users.noreply.github.com>
2021-03-09 19:32:31 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -159,7 +159,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("HELP", callback_data="help_data"),
                     InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")]
+                [InlineKeyboardButton("⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/fCfilmcornerfc")]
             ])
 
             await query.message.edit_text(
